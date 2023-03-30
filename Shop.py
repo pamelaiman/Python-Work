8b1aa7792365cfb14b8041571d733e0454f01a27
Pamela Iman <pamelariman@gmail.com>
2023-03-30 21:10:57 +0100

    committing from terminal

--- Shop.py
+++ Shop.py
@@ -1,33 +1,43 @@
 money_available = 100.00
 
+shop = {'item1': 'iPhone', 'price1': 500.00,
+        'item2': 'Piano', 'price2': 59.00,
+        'item3': 'Lamp', 'price3': 98.00}
 
-def shopping():
-    shop = [
-        {'name': 'iPhone', 'price': 500.00},
-        {'name': 'Wardrobe', 'price': 100.00},
-        {'name': 'Piano', 'price': 59.99},
-        {'name': 'Lamp', 'price': 98.00}]
+print("Welcome to the shop! You have", money_available, "pounds.")
+print("Here is what we have in stock and their prices:")
+print((shop['item1']), 'which costs', (shop['price1']))
+print((shop['item2']), 'which costs', (shop['price2']))
+print((shop['item3']), 'which costs', (shop['price3']))
 
-    print("Welcome to the shop! You have", money_available, "pounds. Here is what we have in stock and their prices:")
-    for item in shop:
-        print((item['name']), 'which costs', (item['price']))
-    choice = input("Do you want to exit the shop, please specify with y or n: ")
-    if choice != 'y' and choice != 'n':
-        raise ValueError('You have not inputted a valid answer')
+# for item in shop:
+#     print(shop[item]) #Attempt to use for loop
 
-    elif choice == 'y':
-        print('You have left the shop.')
+user_exit = input("Do you want to exit the shop, please specify with y or n: ")
 
-    elif choice == 'n':
-        choice2 = input('What item do you want to buy?')
+try:
+    if user_exit == 'y' or user_exit == 'Y':
+        print('You have left the shop. Goodbye.')
 
-        for choice2 in shop:
-            if money_available > choice2['price']:
-                print('You can afford a', choice2['name'])
-
-            #if money_available < choice(int['price']):
-                #print('You cannot afford this')
-
+    elif user_exit == 'n' or user_exit == 'N':
+        choice = input('Great. What item would you like to buy? ')
+        if choice == "iPhone":
+            print('That will cost', {'price': '500.00'}.get('price'), 'pounds')
+            iphone_cost = shop['iPhone']
+            print(iphone_cost)
+            print(item['price'])
 
 
-shopping()
+
+
+        elif choice == "Piano":
+            print('That will cost', {'price': '59.00'}.get('price'), 'pounds')
+
+        elif choice == "Lamp":
+            print('That will cost', {'price': '98.00'}.get('price'), 'pounds')
+
+    else:
+        raise ValueError()
+
+except ValueError as error:
+    print('Error. This is not a valid answer.')
